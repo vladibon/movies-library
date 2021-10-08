b69062b54a7493c0ff7298f356204009ccbf8221
Eduard-Konovka <ed098ua@gmail.com>
2021-10-09 01:50:27 +0300

    Стартовый API servise

---
+++ src/js/api/movies-api.js
@@ -0,0 +1,19 @@
+import imagesApiService from './apiServicePlagin';
+import imageCardTpl from '../../templates/card-markup.hbs';
+
+const imagesContainer = document.querySelector('#js-gallery');
+
+function onSearch() {
+  imagesApiService.fetchArticles().then(createGallery).catch(onFetchError);
+}
+
+function createGallery(images) {
+  console.log(images);
+  imagesContainer.insertAdjacentHTML('beforeend', imageCardTpl(images));
+}
+
+function onFetchError(message) {
+  console.log(message);
+}
+
+onSearch();
