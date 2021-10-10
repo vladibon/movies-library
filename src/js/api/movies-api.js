483631f500f30a5621a3009f1c25fae37594c5d2
Olena-Horobets <80359219+Olena-Horobets@users.noreply.github.com>
2021-10-10 13:33:44 +0300

    fetch data assigns to each film object

--- src/js/api/movies-api.js
+++ src/js/api/movies-api.js
@@ -4,11 +4,17 @@ import imageCardTpl from '../../templates/card-markup.hbs';
 const imagesContainer = document.querySelector('#js-gallery');
 
 function onSearch() {
-  imagesApiService.fetchArticles().then(createGallery).catch(onFetchError);
+  imagesApiService
+    .fetchArticles()
+    .then(data => {
+      const currentPage = getFilmData(data.results);
+
+      createGallery(currentPage);
+    })
+    .catch(onFetchError);
 }
 
 function createGallery(images) {
-  console.log(images);
   imagesContainer.insertAdjacentHTML('beforeend', imageCardTpl(images));
 }
 
@@ -17,3 +23,23 @@ function onFetchError(message) {
 }
 
 onSearch();
+
+function getFilmData(data) {
+  let array = data.map(el => {
+    let film = {
+      id: String(el.id),
+      title: el.title,
+      original_title: el.original_title,
+      poster_path: `http://image.tmdb.org/t/p/w300${el.poster_path}`,
+      vote_average: el.vote_average,
+      vote_count: el.vote_count,
+      overview: el.overview,
+      popularity: el.popularity,
+      release_date: el.release_date.slice(0, 4),
+      genres: 'action, comedy',
+    };
+
+    return film;
+  });
+  return array;
+}
