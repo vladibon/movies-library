514b515e2ea33e94a5b9b97d0bc01c514718b175
Eduard-Konovka <ed098ua@gmail.com>
2021-10-11 02:45:08 +0300

    Фиксит apiServicePlagin

--- src/js/api/trending-movie-search.js
+++ src/js/api/trending-movie-search.js
@@ -1,15 +1,17 @@
-import imagesApiService from './apiServicePlagin';
+import { homeApiService, movieApiService, genresApiService } from './apiServicePlugin';
 import imageCardTpl from '../../templates/card-markup.hbs';
 import localStorage from '../components/local-storage-db';
 import refs from '../components/refs.js';
 
 onTrendingMoviesLoad();
+getOneMovie();
+getGenresMovies();
 
 function onTrendingMoviesLoad() {
-  imagesApiService
+  homeApiService
     .fetchArticles()
     .then(data => {
-      const currentPageMovies = localStorage.getFilmData(data.results);
+      const currentPageMovies = localStorage.getFilmData(data);
       localStorage.saveCurrentPage(currentPageMovies);
       createGallery(currentPageMovies);
     })
@@ -22,4 +24,20 @@ function createGallery(images) {
 
 function onFetchError(message) {
   console.log(message);
+}
+
+// Временая показательная функция для последующего изменения
+function getOneMovie() {
+  movieApiService
+    .fetchArticles()
+    .then(data => console.log('Полная информация о кинофильме для страницы кинофильма:', data))
+    .catch(onFetchError);
+}
+
+// Временая показательная функция для Лены
+function getGenresMovies() {
+  genresApiService
+    .fetchArticles()
+    .then(data => console.log('Жанры фильмов (ДЛЯ ЛЕНЫ):', data))
+    .catch(onFetchError);
 }
