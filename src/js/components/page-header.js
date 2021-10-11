bc733fd1a6576087abfe2e9f7b87ad0b76e2417a
vofm <valerafm.vofm@gmail.com>
2021-10-11 06:33:30 +0300

    Fix refs

--- src/js/components/page-header.js
+++ src/js/components/page-header.js
@@ -1,21 +1,16 @@
 // import onSearch from '../api/movies-api';
+import refs from '../components/refs.js';
 import svgURL from '../../images/sprite.svg';
 
-const getNavRef = document.querySelector('.site-nav__list');
-const getNavHomeRef = document.querySelector('[data-nav="home"]');
-const getNavLibraryRef = document.querySelector('[data-nav="my library"]');
-
-const getSectionHeader = document.querySelector('.section-header');
-
-getNavRef.addEventListener('click', goNavLink);
+refs.menuNav.addEventListener('click', goNavLink);
 
 function goNavLink(e) {
-    if (e.target.classList.contains('site-nav__link--active') === false) {
-        getNavHomeRef.classList.toggle('site-nav__link--active');
-        getNavLibraryRef.classList.toggle('site-nav__link--active');
+    if (!e.target.classList.contains('site-nav__link--active')) {
+        refs.buttonHomeMenu.classList.toggle('site-nav__link--active');
+        refs.buttonLibraryMenu.classList.toggle('site-nav__link--active');
 
         if (e.target.dataset.nav === "home") {
-            getSectionHeader.innerHTML = `
+            refs.sectionHeader.innerHTML = `
             <div class="page-header__search">
                 <input class="page-header__search--input" type="input" placeholder="Поиск фильмов" />
                 <svg class="search-img" width="12" height="12">
@@ -25,7 +20,7 @@ function goNavLink(e) {
             `;
             // onSearch();
         } else {
-            getSectionHeader.innerHTML = `
+            refs.sectionHeader.innerHTML = `
             <div class="page-header__buttons">
                 <button class="btn btn--primary btn__margin js-btn" type="button" data-action="watched">WATCHED</button>
                 <button class="btn btn--primary js-btn" type="button" data-action="queue">QUEUE</button>
--- src/js/components/refs.js
+++ src/js/components/refs.js
@@ -1,3 +1,10 @@
 export default {
   galleryContainer: document.getElementById('js-gallery'),
+
+  // Header Ref
+  menuNav: document.querySelector('.site-nav__list'),
+  buttonHomeMenu: document.querySelector('[data-nav="home"]'),
+  buttonLibraryMenu: document.querySelector('[data-nav="my library"]'),
+  sectionHeader: document.querySelector('.section-header'),
+
 };
