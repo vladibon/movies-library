9bfc5dff8b4e6f9fd6176e0d217c5f9a29af2823
Vladimir Bondar <vladi.bondar@gmail.com>
2021-10-11 16:44:50 +0300

    fix static images adding

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import './sass/main.scss';
 
-import './js/api/movies-api';
 import './js/api/trending-movie-search';
 
 import './js/components/team-markup';
-
--- src/js/components/team-markup.js
+++ src/js/components/team-markup.js
@@ -1,11 +1,6 @@
-import team from '../../team.json';
-
-import teamItems from '../../templates/team-markup.hbs';
+import team from '../db/team.json';
+import teamCardsTemplate from '../../templates/team-markup.hbs';
 
 const teamRef = document.querySelector('.js-team');
-console.log(teamRef);
 
-const teamMarkup = teamItems(team);
-console.log(teamMarkup);
-
-teamRef.insertAdjacentHTML('beforeend', teamMarkup);
+teamRef.insertAdjacentHTML('beforeend', teamCardsTemplate(team));
