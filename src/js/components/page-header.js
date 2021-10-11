1b202d10210ef240aa808386058049d7cfe4063b
vofm <80212894+Valeriy-Fedulov@users.noreply.github.com>
2021-10-11 17:15:28 +0300

    Fix nav

--- src/js/components/page-header.js
+++ src/js/components/page-header.js
@@ -1,16 +1,22 @@
 // import onSearch from '../api/movies-api';
 import refs from '../components/refs.js';
 
-refs.menuNav.addEventListener('click', goNavLink);
+refs.buttonWatched.click();
 
-function goNavLink(e) {
-    console.log(e.target.classList);
-    if (!e.target.classList.contains('site-nav__link--active') || !e.target.classList.contains('site-nav__link--active')) {
-        refs.buttonHomeMenu.classList.toggle('site-nav__link--active');
-        refs.buttonLibraryMenu.classList.toggle('site-nav__link--active');
-
-        refs.sectionHome.classList.toggle('page-header--hidden');
-        refs.sectionMyLibrary.classList.toggle('page-header--hidden');
+refs.menuNav.addEventListener('click', e => {
+    if (e.target.tagName === 'BUTTON') {
+        if (!e.target.children[0].classList.contains('site-nav__link--active')) toggleNav();
     }
+    if (e.target.tagName === 'SPAN') {
+        if (!e.target.classList.contains('site-nav__link--active')) toggleNav();
+    }
+});
+
+function toggleNav() {
+    refs.buttonHomeMenu.classList.toggle('site-nav__link--active');
+    refs.buttonLibraryMenu.classList.toggle('site-nav__link--active');
+    
+    refs.sectionHome.classList.toggle('page-header--hidden');
+    refs.sectionMyLibrary.classList.toggle('page-header--hidden');
 }
 
