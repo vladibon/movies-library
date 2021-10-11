30fe84bda9dcdb6e339afd1fe1c97b94d7886c18
vofm <valerafm.vofm@gmail.com>
2021-10-11 06:43:16 +0300

    Update refs

--- src/js/components/page-header.js
+++ src/js/components/page-header.js
@@ -10,15 +10,10 @@ function goNavLink(e) {
         refs.buttonLibraryMenu.classList.toggle('site-nav__link--active');
 
         if (e.target.dataset.nav === "home") {
-            refs.sectionHeader.innerHTML = ``;
+           
             // onSearch();
         } else {
-            refs.sectionHeader.innerHTML = `
-            <div class="page-header__buttons">
-                <button class="btn btn--primary btn__margin js-btn" type="button" data-action="watched">WATCHED</button>
-                <button class="btn btn--primary js-btn" type="button" data-action="queue">QUEUE</button>
-            </div>
-            `;
+           
         }
     }
 }
--- src/js/components/refs.js
+++ src/js/components/refs.js
@@ -5,6 +5,4 @@ export default {
   menuNav: document.querySelector('.site-nav__list'),
   buttonHomeMenu: document.querySelector('[data-nav="home"]'),
   buttonLibraryMenu: document.querySelector('[data-nav="my library"]'),
-  sectionHeader: document.querySelector('.section-header'),
-
 };
