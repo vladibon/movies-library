bef7c6d8d51236ae7468cebfb0fbae4e0f8b4df1
Vladimir Bondar <vladi.bondar@gmail.com>
2021-10-13 19:56:47 +0300

    fix structure

--- src/js/components/modal-team.js
+++ src/js/components/modal-team.js
@@ -5,7 +5,6 @@ import teamCardsTemplate from '../../templates/team-markup.hbs';
 import team from '../db/team.json';
 
 let teamLightbox = null;
-let teamSplide = null;
 const teamSplideOptions = {
   perPage: 3,
   breakpoints: {
@@ -24,22 +23,25 @@ const teamSplideOptions = {
   // interval: 3000,
 };
 
-refs.footerLink.addEventListener('click', e => {
+refs.footerLink.addEventListener('click', openTeamLightbox);
+
+function openTeamLightbox(e) {
   e.preventDefault();
+
   window.addEventListener('keydown', onKeydown);
 
-  teamLightbox = basicLightbox.create(teamCardsTemplate(team), {
-    onClose: () => window.removeEventListener('keydown', onKeydown),
-  });
+  teamLightbox = basicLightbox.create(teamCardsTemplate(team), { onClose: removeListener });
   teamLightbox.show();
 
-  teamSplide = new Splide('.splide', teamSplideOptions);
+  const teamSplide = new Splide('.splide', teamSplideOptions);
   teamSplide.mount();
-});
+}
 
 function onKeydown(e) {
-  console.log(e);
   if (e.code !== 'Escape') return;
-
   teamLightbox.close();
+}
+
+function removeListener() {
+  window.removeEventListener('keydown', onKeydown);
 }
