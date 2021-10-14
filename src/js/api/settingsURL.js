f6d37886ceded6243204376c6fc130d88fbaa407
Eduard-Konovka <ed098ua@gmail.com>
2021-10-14 12:31:35 +0300

    Вводит объект настроек в функцию запроса к API

--- src/js/api/settingsURL.js
+++ src/js/api/settingsURL.js
@@ -1,6 +1,6 @@
 export default URL = {
   BASE: 'https://api.themoviedb.org/3',
-  KEY: 'api_key=d59d4b143292fc56b6769ba48f713e41',
+  KEY: 'd59d4b143292fc56b6769ba48f713e41',
   W342_IMG_URL: 'https://image.tmdb.org/t/p/w342',
   W500_IMG_URL: 'https://image.tmdb.org/t/p/w500',
   W780_IMG_URL: 'https://image.tmdb.org/t/p/w780',
