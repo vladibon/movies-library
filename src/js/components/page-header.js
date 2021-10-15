25f0b8911561520e94e75b3c813a1c18faca3ef9
Vladimir Bondar <vladi.bondar@gmail.com>
2021-10-15 15:53:23 +0300

    set correct current page pagination rendering

--- src/js/components/page-header.js
+++ src/js/components/page-header.js
@@ -1,7 +1,7 @@
 import renderWatchedMovies from './render-watched-movies';
 import renderQueueMovies from './render-queue-movies';
+import { preloadTrendingMoviesTotalItems } from './render-trending-movies';
 import refs from '../components/refs.js';
-import { resetPaginationPage } from './pagination.js';
 
 refs.menuNav.addEventListener('click', e => {
   if (e.target.tagName === 'BUTTON') {
@@ -28,7 +28,7 @@ function onLogoClick() {
 }
 function loadTrending() {
   refs.input.value = '';
-  resetPaginationPage('home');
+  preloadTrendingMoviesTotalItems();
 
   refs.sectionHome.classList.remove('page-header--hidden');
   refs.sectionMyLibrary.classList.add('page-header--hidden');
--- src/js/components/render-trending-movies.js
+++ src/js/components/render-trending-movies.js
@@ -2,13 +2,13 @@ import { Loading } from 'notiflix';
 import { homeApiService } from '../api/apiServicePlugin';
 import imageCardTpl from '../../templates/card-markup.hbs';
 import dataStorage from './data-storage';
-import { setPaginationTotalItems } from './pagination.js';
+import { setPaginationTotalItems, resetPaginationPage } from './pagination.js';
 import refs from './refs.js';
 
 dataStorage.saveGenresToLS();
 loadTrendingMovies();
 
-export default function loadTrendingMovies() {
+export function loadTrendingMovies() {
   Loading.circle('Loading...');
   homeApiService
     .fetchArticles()
@@ -21,6 +21,16 @@ export default function loadTrendingMovies() {
     })
     .catch(onFetchError)
     .finally(Loading.remove(200));
+}
+
+export function preloadTrendingMoviesTotalItems() {
+  homeApiService
+    .fetchArticles()
+    .then(({ total_results }) => {
+      setPaginationTotalItems(total_results);
+      resetPaginationPage('home');
+    })
+    .catch(console.log);
 }
 
 function createGallery(images) {
